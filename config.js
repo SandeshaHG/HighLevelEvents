74d5e0f5489be89e1a254d14153ed85333335d12
SandeshaHG <sandeshhg1997@gmail.com>
2024-07-28 22:52:31 +0530

    Added static config variables

---
+++ config.js
@@ -0,0 +1,11 @@
+const START_HOURS = "09:00";
+const END_HOURS = "17:00";
+const DURATION = 30;
+const TIMEZONE = "America/Los_Angeles";
+
+module.exports = {
+  START_HOURS,
+  END_HOURS,
+  DURATION,
+  TIMEZONE,
+};
--- routes/events_api.js
+++ routes/events_api.js
@@ -5,4 +5,14 @@ router.get("/", (req, res) => {
   res.send("Get ready to book an appointment with Dr John");
 });
 
+const config = require("../config");
+router.get("/config", (req, res) => {
+  res.json({
+    startHours: config.START_HOURS,
+    endHours: config.END_HOURS,
+    duration: config.DURATION,
+    timezone: config.TIMEZONE,
+  });
+});
+
 module.exports = router;
